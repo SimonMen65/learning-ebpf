2a8e035b06b24f95829d5d9be284712535646ef7
Simon Men <jingyanm@andrew.cmu.edu>
2024-07-03 20:43:55 -0400

    print more

--- disk_latency/disk_latency.c
+++ disk_latency/disk_latency.c
@@ -18,8 +18,8 @@ static int print_latency_mesg(struct disk_latency_bpf *skel){
 	int pid =-1, next_pid;
 	struct data_pid_t m;
 	int err, fd = bpf_map__fd(skel->maps.disk_latency_map);
-	while (!bpf_map__get_next_key(fd, &pid, &next_pid)){
-		err = bpf_map__lookup_elem(fd, &next_pid, &m);
+	while (!bpf_map_get_next_key(fd, &pid, &next_pid)){
+		err = bpf_map_lookup_elem(fd, &next_pid, &m);
 		if (err < 0) {
 			fprintf(stderr, "failed to lookup latency: %d\n", err);
 			return -1;
@@ -30,8 +30,8 @@ static int print_latency_mesg(struct disk_latency_bpf *skel){
 	}
 
 	pid = -1;
-	while (!bpf_map__get_next_key(fd, &pid, &next_pid)){
-		err = bpf_map__delete_elem(fd, &next_pid);
+	while (!bpf_map_get_next_key(fd, &pid, &next_pid)){
+		err = bpf_map_delete_elem(fd, &next_pid);
 		if (err < 0) {
 			fprintf(stderr, "failed to lookup latency: %d\n", err);
 			return -1;
@@ -61,6 +61,7 @@ int main()
 		disk_latency_bpf__destroy(skel);
         return 1;
 	}
+
 
 	while (1){
 		sleep(0.8);
